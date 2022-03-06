d715c92ffccdd90a1c5fc8479998f802cea00a8e
Ralph <Ralph@LAPTOP-OHHP9KOL>
2022-03-06 14:13:47 -0500

    Add project files.

---
+++ LogicRundown/LogicCompares.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicRundown
+{
+    static class LogicCompares
+    {
+        public static bool CompareLessThan(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return (InvertBoolValue((Inverter), (AlphaValue < BetaValue)));
+
+        }
+
+        public static bool CompareDividedBy(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return (InvertBoolValue((Inverter), (BetaValue % AlphaValue == 0)));
+
+        }
+
+        public static bool CompareInBetween(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return (InvertBoolValue((Inverter), (Math.Pow(AlphaValue, BetaValue) > 200 && Math.Pow(AlphaValue, BetaValue) < 300)));
+            
+        }
+
+        public static bool CompareGreaterEven(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return ((AlphaValue * BetaValue) > 10 != ((AlphaValue % 2 == 0) && (BetaValue % 2 == 0)));
+            
+        }
+
+        public static bool ComparePrime(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            int counter = 0;
+            for (int i = 1; i <= AlphaValue; i++)
+            {
+                if (AlphaValue % i == 0)
+                {
+                    counter++;
+                }
+
+            }
+            return (counter == 2);
+
+        }
+
+        public static bool CompareNice(int AlphaValue, int BetaValue, bool Inverter)
+        {
+            return (BetaValue == 69);
+        }
+        private static bool InvertBoolValue(bool shouldInvert, bool assessment)
+        {
+            return (shouldInvert ? !assessment : assessment);
+        }
+    }
+}
---
+++ LogicRundown/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogicRundown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //variables
+        private int AlphaValue = 0;
+
+        private int BetaValue = 0;
+
+        //property
+        private bool Inverter
+        {
+            get
+            {
+                return (bool)InvertLogicCheckBox.IsChecked;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AlphaTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (AlphaTextBox.Text.Length > 0 && BetaTextBox.Text.Length > 0)
+            {
+                CheckValuesButton.IsEnabled = true; //Will explain this during refactoring
+            }
+            else
+            {
+                CheckValuesButton.IsEnabled = false; //Will explain this during refactoring
+            }
+        }
+
+        private void BetaTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (AlphaTextBox.Text.Length > 0 && BetaTextBox.Text.Length > 0)
+            {
+                CheckValuesButton.IsEnabled = true; //Will explain this during refactoring
+            }
+            else
+            {
+                CheckValuesButton.IsEnabled = false; //Will explain this during refactoring
+            }
+        }
+
+        private static bool InvertBoolValue(bool shouldInvert, bool assessment)
+        {
+            return (shouldInvert ? !assessment : assessment);
+        }
+
+        private void CheckValuesButton_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(AlphaTextBox.Text, out AlphaValue);
+            int.TryParse(BetaTextBox.Text, out BetaValue);
+            if (AlphaValue == 0 || BetaValue == 0)
+            {
+                MessageBox.Show("Change the default values - No divide by zero please.");
+            }
+            else
+            {
+                if (LogicCompares.CompareLessThan(AlphaValue, BetaValue, Inverter))
+                {
+                    LogicCanvasOne.Background = new SolidColorBrush(Colors.Green);
+                }
+                else
+                {
+                    LogicCanvasOne.Background = new SolidColorBrush(Colors.Red);
+                }
+            }
+
+            if (LogicCompares.CompareDividedBy(AlphaValue, BetaValue, Inverter))
+            {
+                LogicCanvasTwo.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                LogicCanvasTwo.Background = new SolidColorBrush(Colors.Red);
+            }
+
+            if (LogicCompares.CompareInBetween(AlphaValue, BetaValue, Inverter))
+            {
+                LogicCanvasThree.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                LogicCanvasThree.Background = new SolidColorBrush(Colors.Red);
+            }
+
+            //Ho ho ho - Now I got a Machine Gun
+            if (LogicCompares.CompareGreaterEven(AlphaValue, BetaValue, Inverter))
+            {
+                LogicCanvasXor.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                LogicCanvasXor.Background = new SolidColorBrush(Colors.Red);
+            }
+
+            if (LogicCompares.ComparePrime(AlphaValue, BetaValue, Inverter))
+            {
+                LogicCanvasFive.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                LogicCanvasFive.Background = new SolidColorBrush(Colors.Red);
+            }
+            if (LogicCompares.CompareNice(AlphaValue, BetaValue, Inverter))
+            {
+                LogicCanvasSix.Background = new SolidColorBrush(Colors.Green);
+                NiceBlock.Foreground = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                LogicCanvasSix.Background = new SolidColorBrush(Colors.Red);
+                NiceBlock.Foreground = new SolidColorBrush(Colors.Red);
+
+            }
+        }
+
+
+
+
+
+
+        private void CheckValuesButton_MouseStuff(object sender, MouseEventArgs e)
+        {
+            backy.Focus();
+        }
+
+        private void AlphaTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AlphaTextBox.Text = string.Empty;
+        }
+
+        private void BetaTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            BetaTextBox.Text = string.Empty;
+        }
+    }
+}
